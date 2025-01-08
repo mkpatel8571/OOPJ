09d393c474ca8577ad8d771769e4b65d74836b04
mkpatel8571 <mkpatel8571@gmail.com>
2025-01-08 16:55:33 +0530

    JAVA

---
+++ areaofsquare.java
@@ -0,0 +1,14 @@
+//In a program, input the side of a square. You have to output the area of 
+//the square.
+import java.util.*;
+public class areaofsquare {
+   public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter a side :");
+    int side=sc.nextInt();
+    int area=4*side;
+    System.out.println(area);
+    
+
+   } 
+}
